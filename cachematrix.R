409f2d73a282054fae13e3ad72ee691bf3862dca
Soumya072002 <soumya072002@gmail.com>
2021-01-03 11:10:42 +0530

    Written the code for the 2 functions A. makeCacheMatrix B.CacheSolve
    Also updated the description of the different functions as requried

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating a function to carry out Matrix Inversion for square (2x2)matrix
+## The function makeCacheMatrix will generate the inverse matrix of the input matrix
+## In order to retrieve the inverse matrix for an already generated one 
+## we will use another function cacheSolve , R will check if there is 
+## data available in cache (via cacheSolve function)-will retrieve if available
+## will generate if not available & feed makeCacheMatrix function
 
-## Write a short comment describing this function
+## function makeCacheMatrix will generate the inverse matrix of the input matrix
+## R will check if there is data available in cache (via cacheSolve function)-
+## will retrieve if available
+## will generate if not available & feed makeCacheMatrix function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- NULL #Named variable as inv_mat for Inverted Matrix
+    set_mat <- function(y) {
+    x<<- y
+    inv_mat <<- NULL
+    }
+    get_mat <- function()x #Getting the Matrix data from set_mat
+    set_matinv <-function(solve) inv_mat<<- solve
+    get_matinv <-function()inv_mat
+    list(set_mat = set_mat, get_mat = get_mat, 
+         set_matinv = set_matinv, get_matinv = get_matinv )
 }
 
-
-## Write a short comment describing this function
+## In order to retrieve the inverse matrix for an already generated one 
+## we will use another function cacheSolve , R will check if there is 
+## data available in cache (via cacheSolve function)-will retrieve if available
+## will generate if not available & feed makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       inv_mat <- x$get_matinv()
+       if (!is.null(inv_mat)) {
+       message( "getting cached inverted matrix")
+       return(inv_mat)
+       }
+       data_mat <- x$get_mat()
+       inv_mat <- solve(data_mat, ...)
+       inv_mat ## Return a matrix that is the inverse of 'x'
 }
